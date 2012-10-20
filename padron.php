179472d63951125d15cded3e89c371717ffa5263
Mario Lamas <mlamases@gmail.com>
2012-10-20 02:01:43 -0500

    Centro del mapa de las búsquedas

---
+++ padron.php
@@ -0,0 +1,20 @@
+<?php
+
+$ubigeo = $_GET['ubigeo'];
+//codDreUgel:
+$codCentroPoblado = $_GET['codCentroPoblado'];
+$nomCentroPoblado = $_GET['nomCentroPoblado'];
+$codigoModular = $_GET['codigoModular'];
+$nombreIE = $_GET['nombreIE'];
+$codigoLocal = $_GET['codigoLocal'];
+$direccionIE = $_GET['direccionIE'];
+//progarti:
+//progise:
+$gestiones = $_GET['gestiones'];
+//areas:
+
+$url = "http://escale.minedu.gob.pe/padron/rest/iiee?ubigeo=" . $ubigeo . "&codCentroPoblado=" . $codCentroPoblado . "&nomCentroPoblado=" . $nomCentroPoblado . "&codigoModular=" . $codigoModular . "&nombreIE=" . $nombreIE . "&codigoLocal=" . $codigoLocal . "&direccionIE=" . $direccionIE . "&gestiones=" . $gestiones;
+
+$xml = simplexml_load_file($url);
+echo json_encode($xml);
+?>
